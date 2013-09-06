e08b74937e651b29266b8a3d3e425871037bcbf4
Alexandra Nesterova <alex.nesterova@gmail.com>
2013-09-06 21:53:50 +0500

    Design changes in OS Migration Tool

--- DriversGalaxy.OSMigrationTool.Backup/MainWindow.xaml.cs
+++ DriversGalaxy.OSMigrationTool.Backup/MainWindow.xaml.cs
@@ -4,6 +4,9 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Markup;
 using DriversGalaxy.OSMigrationTool.Backup.ViewModels;
+using System.Windows.Media.Animation;
+using DriversGalaxy.OSMigrationTool.Backup.Infrastructure;
+using System.Diagnostics;
 
 namespace DriversGalaxy.OSMigrationTool.Backup
 {
@@ -28,6 +31,69 @@ namespace DriversGalaxy.OSMigrationTool.Backup
 			var model = new MainWindowViewModel();
 			DataContext = model;
 		}
+
+        /// <summary>
+        /// Opens an AboutBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OpenAboutBox(object sender, RoutedEventArgs e)
+        {
+            AboutBox aboutBox = new AboutBox { Owner = this };
+
+            aboutBox.Left = Left + (Width / 2 - aboutBox.Width / 2);
+            var regToolsHeight = (int)aboutBox.Height;
+            aboutBox.Height = 0;
+            int topStart = (int)(Top + Height) + 30;
+            aboutBox.Top = topStart;
+            var topFinal = (int)(Top + (Height / 2 - regToolsHeight / 2));
+
+            const int fullAnimationDuration = 300;
+            int heightAnimationDuration = (fullAnimationDuration * regToolsHeight / (topStart - topFinal));
+
+            var slideUp = new DoubleAnimation
+            {
+                From = topStart,
+                To = topFinal,
+                Duration = new Duration(TimeSpan.FromMilliseconds(fullAnimationDuration))
+            };
+            aboutBox.BeginAnimation(TopProperty, slideUp);
+
+            var scaleUp = new DoubleAnimation
+            {
+                From = 0,
+                To = regToolsHeight,
+                Duration = new Duration(TimeSpan.FromMilliseconds(heightAnimationDuration))
+            };
+            aboutBox.BeginAnimation(HeightProperty, scaleUp);
+
+            aboutBox.AnimateInnerBox();
+
+            aboutBox.ShowDialog();
+        }
+
+
+        /// <summary>
+        /// Opens the root URL of the Freemium Utilites website
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OpenRootUrl(object sender, RoutedEventArgs e)
+        {
+            OpenUrl(WPFLocalizeExtensionHelpers.GetUIString("RootUrl"), e);
+        }
+
+        /// <summary>
+        /// Opens a specified URL with a system default browser
+        /// </summary>
+        /// <param name="url">URL to open</param>
+        /// <param name="e"></param>
+        public void OpenUrl(string url, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(url));
+            e.Handled = true;
+        }
+
 
 		#region Window operations
 
--- DriversGalaxy.OSMigrationTool.Restore/MainWindow.xaml.cs
+++ DriversGalaxy.OSMigrationTool.Restore/MainWindow.xaml.cs
@@ -4,6 +4,9 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Markup;
 using DriversGalaxy.OSMigrationTool.Restore.ViewModels;
+using DriversGalaxy.OSMigrationTool.Restore.Infrastructure;
+using System.Diagnostics;
+using System.Windows.Media.Animation;
 
 namespace DriversGalaxy.OSMigrationTool.Restore
 {
@@ -24,6 +27,69 @@ namespace DriversGalaxy.OSMigrationTool.Restore
 				Application.Current.MainWindow.Resources.MergedDictionaries.Add(dic);
 			}
 		}
+
+
+        /// <summary>
+        /// Opens an AboutBox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OpenAboutBox(object sender, RoutedEventArgs e)
+        {
+            AboutBox aboutBox = new AboutBox { Owner = this };
+
+            aboutBox.Left = Left + (Width / 2 - aboutBox.Width / 2);
+            var regToolsHeight = (int)aboutBox.Height;
+            aboutBox.Height = 0;
+            int topStart = (int)(Top + Height) + 30;
+            aboutBox.Top = topStart;
+            var topFinal = (int)(Top + (Height / 2 - regToolsHeight / 2));
+
+            const int fullAnimationDuration = 300;
+            int heightAnimationDuration = (fullAnimationDuration * regToolsHeight / (topStart - topFinal));
+
+            var slideUp = new DoubleAnimation
+            {
+                From = topStart,
+                To = topFinal,
+                Duration = new Duration(TimeSpan.FromMilliseconds(fullAnimationDuration))
+            };
+            aboutBox.BeginAnimation(TopProperty, slideUp);
+
+            var scaleUp = new DoubleAnimation
+            {
+                From = 0,
+                To = regToolsHeight,
+                Duration = new Duration(TimeSpan.FromMilliseconds(heightAnimationDuration))
+            };
+            aboutBox.BeginAnimation(HeightProperty, scaleUp);
+
+            aboutBox.AnimateInnerBox();
+
+            aboutBox.ShowDialog();
+        }
+
+        /// <summary>
+        /// Opens the root URL of the Freemium Utilites website
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void OpenRootUrl(object sender, RoutedEventArgs e)
+        {
+            OpenUrl(WPFLocalizeExtensionHelpers.GetUIString("RootUrl"), e);
+        }
+
+        /// <summary>
+        /// Opens a specified URL with a system default browser
+        /// </summary>
+        /// <param name="url">URL to open</param>
+        /// <param name="e"></param>
+        public void OpenUrl(string url, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(url));
+            e.Handled = true;
+        }
+
 
 		#region Window operations
 
